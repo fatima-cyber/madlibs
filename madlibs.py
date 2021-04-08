92e9092d22b8077c958e1fdcb6773cfb8ab3f28f
fatima-cyber <81115296+fatima-cyber@users.noreply.github.com>
2021-04-08 13:03:08 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,32 @@
+
+
+
+# MadLibs - String Concatenation
+
+#suppose we want to create a string that says "subscribe to ____" 
+youtuber = "Fatima" #some string variable
+
+# a few ways to do this
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}") # using this
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+verb3 = input("verb: ")
+verb4 = input("verb: ")
+verb5 = input("verb: ")
+verb6 = input("verb: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}! My favorite thing \
+to do in the morning is {verb3} and {verb4}. I want youto know that you are capable of \
+{verb5} if you put your mind to it. The {verb6} about trying is get to find your strengths \
+and weakness and learn how to grow.
+
+print(madlib)
+    
